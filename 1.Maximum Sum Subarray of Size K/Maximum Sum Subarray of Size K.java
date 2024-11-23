43992fc87f2daad41ed6d4947dc65746a6cda3bc
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-11-23 07:07:07 +0530

    Maximum Sum Subarray of Size K.java

--- 1.Maximum Sum Subarray of Size K/Maximum Sum Subarray of Size K.java
+++ 1.Maximum Sum Subarray of Size K/Maximum Sum Subarray of Size K.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int maximumSumSubarray(int[] arr, int k) {
+        // Code here
+        int sum=0;
+        int windowsum=0;
+        int start=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+           sum+=arr[i];
+           if(i-start+1==k){
+               windowsum=Math.max(windowsum,sum);
+               sum-=arr[start];
+               start++;
+           }
+           
+        }
+        return windowsum;
+    }
+}
